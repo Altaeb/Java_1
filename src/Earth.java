fe39ccb057c4549e1571c96acd008316f733e8af
Abdelfattah Altaeb <42971313+aftiyah94@users.noreply.github.com>
2019-02-27 10:15:14 +0200

    first commit

---
+++ src/Earth.java
@@ -0,0 +1,11 @@
+public class Earth {
+    public static void main(String[] args) {
+        Human tom;
+        tom = new Human();
+        tom.name="abdalfayyah altaeb";
+        tom.age=24;
+        tom.Color="Green";
+        tom.higthInInches=105;
+        tom.speak();
+    }
+}
---
+++ src/Human.java
@@ -0,0 +1,21 @@
+public class Human {
+    String name;
+    int age;
+    int higthInInches;
+    String Color;
+    public void speak(){
+        System.out.println("My name is "+name);
+        System.out.println("I am "+age+" years old");
+        System.out.println("I am "+higthInInches+" inches tall");
+        System.out.println("My eye color is "+Color);
+    }
+    public void eat() {
+        System.out.println("eating .. ");
+    }
+    public void walk() {
+        System.out.println("walking ..");
+    }
+    public void work(){
+        System.out.println("working ..");
+    }
+}
